fbdc5fcf8de7344605d701d60980a9e504b98b2d
MerajCodes <113118915+merajCodes3440@users.noreply.github.com>
2023-06-10 12:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function getSize(){
+    var w = window.innerWidth;
+	var h = window.innerHeight;
+
+	document.getElementById("sizeInfo").innerHTML="<h1> width: "+w+" and height: "+h+"</h1"
+}
